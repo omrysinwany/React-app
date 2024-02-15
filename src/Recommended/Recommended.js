6703fce98cf57f24b8654682036fa83eca775d83
Omry Sinwany <Omrysinwanyy@gmail.com>
2024-02-16 00:50:58 +0200

    1.0

---
+++ src/Recommended/Recommended.js
@@ -0,0 +1,21 @@
+import Button from "../components/Button";
+import "./Recommended.css";
+
+const Recommended = ({ handleClick }) => {
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          <Button onClickHandler={handleClick} value="" title="All Products" />
+          <Button onClickHandler={handleClick} value="Nike" title="Nike" />
+          <Button onClickHandler={handleClick} value="Puma" title="Puma" />
+          <Button onClickHandler={handleClick} value="Vans" title="Vans" />
+          <Button onClickHandler={handleClick} value="HS" title="HS" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Recommended;
